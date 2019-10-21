d18a704ed21ffad5c8b95a4929d5fe9941307561
Annie Truex <artruex@gmail.com>
2019-10-21 04:22:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,14 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-    
-    get '/' do 
+    get '/' do
       erb :root
     end
-    
-    get '/new' do 
+
+    get '/new' do
       erb :'pirates/new'
     end
-    
+
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
